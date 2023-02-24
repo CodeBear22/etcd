d6f2b92203a71eed6391efb410930170f383e6ee
Kunal Soni <kunal.k.soni@oracle.com>
2023-02-24 21:18:23 +0530

    fmt check

--- test.sh
+++ test.sh
@@ -374,13 +374,16 @@ function fmt_pass {
   # after resolving ore-existing errors.
   # markdown_you  -  too sensitive check was temporarilly disbled. 
   #revive shellcheck
-  for p in goword \
+  for p in shellcheck \
+      goword \
       gofmt \
       govet \
+      revive \
       license_header \
       receiver_name \
       mod_tidy \
       dep \
+      shellcheck \
       shellws \
       ; do
     run_pass "${p}" "${@}"
